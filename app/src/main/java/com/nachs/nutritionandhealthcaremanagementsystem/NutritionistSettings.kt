18f916b2547c11d8982dcaa3b6f5cf082c432ddc
Edmund <edmundlzen@gmail.com>
2022-12-06 19:45:51 +0800

    Added missing onClickNotifications in NutritionistSettings.kt

--- app/src/main/java/com/nachs/nutritionandhealthcaremanagementsystem/NutritionistSettings.kt
+++ app/src/main/java/com/nachs/nutritionandhealthcaremanagementsystem/NutritionistSettings.kt
@@ -3,6 +3,7 @@ package com.nachs.nutritionandhealthcaremanagementsystem
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 
 class NutritionistSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,10 @@ class NutritionistSettings : AppCompatActivity() {
 
     fun onClickBackButton(view: View) {
         onBackPressed()
+    }
+
+    fun onClickNotifications(view: View) {
+        val switch: SwitchCompat = findViewById(R.id.switchNotifications)
+        switch.isChecked = !switch.isChecked
     }
 }
