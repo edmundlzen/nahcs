d6a5d7f204c8186208e3aebf9b991d747f856a51
Edmund <edmundlzen@gmail.com>
2022-12-07 19:38:24 +0800

    Added posts

--- app/src/main/java/com/nachs/nutritionandhealthcaremanagementsystem/Home.kt
+++ app/src/main/java/com/nachs/nutritionandhealthcaremanagementsystem/Home.kt
@@ -3,12 +3,53 @@ package com.nachs.nutritionandhealthcaremanagementsystem
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Generate a lot of fake rows
+        val linearLayout = findViewById<LinearLayout>(R.id.llPosts)
+        val nutritionists = arrayOf(
+            "Sandy",
+            "Tina",
+            "Sally",
+            "Sue",
+            "Samantha",
+            "Sara",
+            "Sasha",
+            "Sandra",
+            "Quinn",
+        )
+        val titles = arrayOf(
+            "How to eat healthy",
+            "How to lose weight",
+            "How to gain weight",
+            "How to cure cancer",
+            "How to cure diabetes",
+            "How to cure heart disease",
+            "How to cure obesity",
+            "How to cure depression",
+            "How to cure anxiety",
+        )
+        for (i in 0..8) {
+            val view =
+                layoutInflater.inflate(R.layout.activity_home, null, false)
+            val nutritionist = view.findViewById<TextView>(R.id.tvNutritionistName1)
+            val title = view.findViewById<TextView>(R.id.tvTitlePost1)
+            val date = view.findViewById<TextView>(R.id.tvDate1)
+            nutritionist.text = nutritionists[i]
+            title.text = titles[i]
+            date.text = "${i + 2} days ago"
+
+            val post = view.findViewById<LinearLayout>(R.id.llContentPost1)
+            (post.parent as LinearLayout).removeView(post)
+            linearLayout.addView(post)
+        }
     }
 
     fun onClickSettingsButton(view: View) {
@@ -19,6 +60,11 @@ class Home : AppCompatActivity() {
         } else {
             Intent(this, MemberSettings::class.java)
         }
+        startActivity(intent)
+    }
+
+    fun onClickBMICalculatorButton(view: View) {
+        val intent = Intent(this, BMICalculation::class.java)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/nachs/nutritionandhealthcaremanagementsystem/UserTypeSelection.kt
+++ app/src/main/java/com/nachs/nutritionandhealthcaremanagementsystem/UserTypeSelection.kt
@@ -2,27 +2,37 @@ package com.nachs.nutritionandhealthcaremanagementsystem
 
 import android.app.ActivityOptions
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class UserTypeSelection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_type_selection)
+
+        val userIsLoggedIn =
+            applicationContext.getSharedPreferences("prefs", MODE_PRIVATE).getString("username", "")
+
+        if (userIsLoggedIn != "") {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
     }
 
     fun onClickUser(view: View) {
         val logoView: View = findViewById<View>(R.id.ivLogo)
         val intent: Intent = Intent(this, UserOnboard::class.java)
-        val options: ActivityOptions = ActivityOptions.makeSceneTransitionAnimation(this, logoView, "logo")
+        val options: ActivityOptions =
+            ActivityOptions.makeSceneTransitionAnimation(this, logoView, "logo")
         startActivity(intent, options.toBundle())
     }
 
     fun onClickNutritionist(view: View) {
         val logoView: View = findViewById<View>(R.id.ivLogo)
         val intent: Intent = Intent(this, UserLogin::class.java)
-        val options: ActivityOptions = ActivityOptions.makeSceneTransitionAnimation(this, logoView, "logo")
+        val options: ActivityOptions =
+            ActivityOptions.makeSceneTransitionAnimation(this, logoView, "logo")
         startActivity(intent, options.toBundle())
     }
 }
